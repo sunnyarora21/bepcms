95ba5948065d93b018b9ab7b2f4e7e850dedc214
MEAN Hive <meanhive@zoho.com>
2020-01-05 22:26:04 +0530

    Update router.controller.js

--- router/router.controller.js
+++ router/router.controller.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const courseRoute = require('./course.router');
-
+const courseModuleRoute = require('./coursemodule.router');
 app.use('/course', courseRoute);
+app.use('/coursemodule', courseModuleRoute);
 
 module.exports = app;
