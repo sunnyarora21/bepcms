735a4ee4d4230784816453785285888688572b0b
sunnyarora21 <33170949+sunnyarora21@users.noreply.github.com>
2020-02-02 16:29:39 +0530

    Update Db.js

--- config/Db.js
+++ config/Db.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB:
-    'mongodb+srv://pruthvi_dharani:Pruthvi143@ganesh-nlnmw.mongodb.net/dbbepractical?retryWrites=true&w=majority'
+    'mongodb+srv://deepak:test1234@cluster0-qmj1p.mongodb.net/dbbepractical?retryWrites=true&w=majority'
 };
